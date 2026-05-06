fd15c45c7d8840528c70a79a20a411ab9363846e
Olga Rekun <rekun.olya@gmail.com>
2026-05-06 17:28:37 +0200

    feat: add header to project

--- src/app/features/components/header/header.ts
+++ src/app/features/components/header/header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,4 +6,6 @@ import { Component } from '@angular/core';
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {}
+export class Header {
+  title = signal('Film collection');
+}
